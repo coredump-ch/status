2f45a2eec7caa36e37ed681754690942c837c2ea
Danilo Bargen <mail@dbrgn.ch>
2015-04-11 18:23:04 +0200

    Moved get_port function to utils module

--- status/src/main.rs
+++ status/src/main.rs
@@ -1,11 +1,13 @@
+//! The main entry point for the SpaceAPi server.
+
 extern crate rustc_serialize;
 extern crate hyper;
 
 mod spaceapi;
+mod utils;
 
 use std::io::Write;
 use std::net::Ipv4Addr;
-use std::env;
 
 use rustc_serialize::json;
 use hyper::Server;
@@ -64,22 +66,9 @@ fn status_endpoint(_: Request, res: Response<Fresh>) {
     res.end().unwrap();
 }
 
-/// Read the port number from the PORT env variable.
-/// If the variable is not set or cannot be parsed to u16,
-/// use 3000 as default port.
-fn get_port() -> u16 {
-    match env::var("PORT") {
-        Ok(val) => match val.parse::<u16>() {
-            Ok(val) => val,
-            Err(_) => 3000
-        },
-        Err(_) => 3000
-    }
-}
-
 fn main() {
     let ip = Ipv4Addr::new(127, 0, 0, 1);
-    let port = get_port();
+    let port = utils::get_port();
 
     println!("Starting HTTP server on {}:{}...", ip, port);
     Server::http(status_endpoint).listen((ip, port)).unwrap();
