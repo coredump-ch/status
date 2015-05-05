91c33c7c023af83eafb36d2f237c75a969df785c
Danilo Bargen <mail@dbrgn.ch>
2015-05-05 21:21:23 +0200

    Added datastore interface

---
+++ status/src/datastore.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+use std::error::Error;
+
+use redis;
+
+
+/// A DataStore needs to implement store and retrieve methods.
+pub trait DataStore {
+    fn store(&self, key: &str, value: &str) -> Result<(), DataStoreError>;
+    fn retrieve(&self, key: &str) -> Result<&str, DataStoreError>;
+}
+
+/// A struct representing a datastore error.
+#[derive(Debug)]
+pub struct DataStoreError {
+    repr: ErrorKind,
+}
+
+/// An enum containing all possible error kinds.
+#[derive(Debug)]
+enum ErrorKind {
+    RedisError(redis::RedisError),
+}
+
+impl From<redis::RedisError> for DataStoreError {
+    fn from(err: redis::RedisError) -> DataStoreError {
+        DataStoreError { repr: ErrorKind::RedisError(err) }
+    }
+}
